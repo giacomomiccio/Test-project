e387bb33a48ff6eb994f10f748477b02624596b5
giacomomiccio <giacomomiccio@fastmail.fm>
2021-12-29 23:23:07 +0100

    Add files via upload

---
+++ Plane.java
@@ -0,0 +1,17 @@
+package com.demos.machines.vehicles
+public class Plane
+{
+
+	public takeoff (DateTime schedule, int lane, String priority)
+	{
+
+	}
+
+	public planeservices (String category)
+	{
+
+	}
+
+
+
+}
